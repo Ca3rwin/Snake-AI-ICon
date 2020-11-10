b780845af0e59508b91ffe5801726b136795e4d3
Ca3rwin <ivan.valluzzi@gmail.com>
2020-11-10 20:23:46 +0100

    Traduzione neural_network

--- neural_network.py
+++ neural_network.py
@@ -1,19 +1,13 @@
-# Valentin Mac√©
-# valentin.mace@kedgebs.com
-# Developed for fun
-# Feel free to use this code as you wish as long as you quote me as author
-
 """
 neural_network.py
 ~~~~~~~~~~
 
-This module is for building a classic dense neural network
+Questo modulo Ë utilizzato per la costruzione di DNN
 
-Weights and biases are initialized randomly according to a normal distribution
-A network can be saved and loaded for later use
-The class is build for the snake project so:
-- The rendering method is not very modular and is specifi for this project, I'll improve it later
-- No backpropagation since we don't need it for the genetic algorithm
+Pesi e bias sono inizializzati casualmente secondo una distribuzione di Gauss
+Una neural net puo' essere salvata e caricata a piacimento
+
+Non viene implementata la back-propagation dato che utilizzeremo l'algoritmo genetico
 """
 
 from numba import jit
@@ -24,28 +18,30 @@ from pygame import gfxdraw
 
 
 class NeuralNetwork:
-    """Neural Network class"""
+    """ Neural Network class """
 
     def __init__(self, shape=None):
         """
-        :param shape: list of int, describes how many layers and neurons by layer the network has
+        Funzione d'inizializzazione
+        
+        :param shape: lista di int, descrive i layer e i neuroni per layer della neural net
         """
         self.shape = shape
         self.biases = []
         self.weights = []
-        self.score = 0        # to remember how well it performed
+        self.score = 0        # punteggio per sapere quanto ha giocato bene
         if shape:
-            for y in shape[1:]:                             # biases random initialization
+            for y in shape[1:]:                             # inizializzazione casuale dei bias
                 self.biases.append(np.random.randn(y, 1))
-            for x, y in zip(shape[:-1], shape[1:]):         # weights random initialization
+            for x, y in zip(shape[:-1], shape[1:]):         # inizializzazione casuale dei pesi
                 self.weights.append(np.random.randn(y, x))
 
     def feed_forward(self, a):
         """
-        Main function, takes an input vector and calculate the output by propagation through the network
+        Funzione principale, prende un vettore input e ne calcola l'output tramite forward-propagation
 
-        :param a: column of integers, inputs for the network (snake's vision)
-        :return: column of integers, output neurons activation
+        :param a: colonna di integer, input per la neural net (visione del serpente)
+        :return: colonna di integer, attivazione dei neuroni output
         """
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
@@ -53,10 +49,10 @@ class NeuralNetwork:
 
     def save(self, name=None):
         """
-        Saves network weights and biases into 2 separated files in current folder
+        Salva i pesi e bias in due file separati nella cartella attuale
 
-        :param name: str, in case you want to name it
-        :return: creates two files
+        :param name: stringa, in caso volessi darle un nome
+        :return: crea due file
         """
         if not name:
             np.save('saved_weights_'+str(self.score), self.weights)
@@ -67,52 +63,52 @@ class NeuralNetwork:
 
     def load(self, filename_weights, filename_biases):
         """
-        Loads saved network weights and biases from 2 files into the actual network object
+        Carica pesi e bias di neural net salvate da 2 file all'interno dell'oggetto neural net
 
-        :param filename_weights: file containing saved weights
-        :param filename_biases: file containing saved biases
+        :param filename_weights: file contenente i pesi salvati
+        :param filename_biases: file contenente i bias salvati
         """
         self.weights = np.load(filename_weights, allow_pickle=True)
         self.biases = np.load(filename_biases, allow_pickle=True)
 
     def render(self, window, vision):
         """
-        Display the network at the current state in the right part of game window
+        Mostra la neural net allo stato corrente nella parte destra della finestra di gioco
 
-        The function supports any network shape but is not very flexible
+        La funzione supporta qualsiasi forma di neural net fintantoche' i neuroni di input e output siano quelli giusti (21 di input, 3 di output)
         I plan to work on it for later projects
 
-        :param window: surface, game window
-        :param vision: column of int, snake vision needed to show inputs
+        :param window: finestra di gioco
+        :param vision: lista di int, "vista" del serpente necessaria a mostrare gli input
         """
-        network = [np.array(vision)]            # will contain all neuron activation from each layer
+        network = [np.array(vision)]            # conterra' tutte le attivazioni dei neuroni per ogni layer
         for i in range(len(self.biases)):
-            activation = sigmoid(np.dot(self.weights[i], network[i]) + self.biases[i])  # compute neurons activations
-            network.append(activation)                                                  # append it
+            activation = sigmoid(np.dot(self.weights[i], network[i]) + self.biases[i])  # elabora l'attivazione dei neuroni
+            network.append(activation)                                                  # la mette in append
 
-        screen_division = WINDOW_SIZE / (len(network) * 2)     # compute distance between layers knowing window's size
+        screen_division = WINDOW_SIZE / (len(network) * 2)     # elabora la distanza fra i layer relativamente alle dimensioni della finestra
         step = 1
-        for i in range(len(network)):                                           # for each layer
-            for j in range(len(network[i])):                                    # for each neuron in current layer
-                y = int(WINDOW_SIZE/2 + (j*24) - (len(network[i])-1)/2 * 24)    # neuron position
+        for i in range(len(network)):                                           # per ogni layer
+            for j in range(len(network[i])):                                    # e per ogni neurone nel layer attuale
+                y = int(WINDOW_SIZE/2 + (j*24) - (len(network[i])-1)/2 * 24)    # posizione del neurone
                 x = int(WINDOW_SIZE + screen_division * step)
-                intensity = int(network[i][j][0] * 255)                         # neuron intensity
+                intensity = int(network[i][j][0] * 255)                         # intensita' del neurone
 
                 if i < len(network)-1:
-                    for k in range(len(network[i+1])):                                          # connections
-                        y2 = int(WINDOW_SIZE/2 + (k * 24) - (len(network[i+1]) - 1) / 2 * 24)   # connections target position
+                    for k in range(len(network[i+1])):                                          # archi
+                        y2 = int(WINDOW_SIZE/2 + (k * 24) - (len(network[i+1]) - 1) / 2 * 24)   # posizione target degli archi
                         x2 = int(WINDOW_SIZE + screen_division * (step+2))
-                        pygame.gfxdraw.line(window, x, y, x2, y2,                               # draw connection
+                        pygame.gfxdraw.line(window, x, y, x2, y2,                               # renderizza la connessione
                                             (intensity/2+30, intensity/2+30, intensity/2+30, intensity/2+30))
 
-                pygame.gfxdraw.filled_circle(window, x, y, 9, (intensity, intensity, intensity))    # draw neuron
+                pygame.gfxdraw.filled_circle(window, x, y, 9, (intensity, intensity, intensity))    # renderizza il neurone
                 pygame.gfxdraw.aacircle(window, x, y, 9, (205, 205, 205))
             step += 2
 
 @jit(nopython=True)
 def sigmoid(z):
     """
-    The sigmoid function, classic neural net activation function
-    @jit is used to speed up computation
+    Funzione sigmoidea, per l'attivazione dei neuroni
+    @jit e' utilizzato per velocizzare l'elaborazione
     """
     return 1.0 / (1.0 + np.exp(-z))
